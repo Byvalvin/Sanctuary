9dc8011504a65a9c66b6aac7aa2b608728520a1e
Byvalvin <usertakeny@gmail.com>
2023-02-27 13:23:14 -0700

    LOS lastmin fixes+
    - The way the light was interacting with some objects was bothering me. fixed
    -Added southpaw movement: ESDF
    -Colliders: It looks like the cat is walking on the top of objects at times. fixed

---
+++ Sanctuary/js/plugins/ESDF.js
@@ -0,0 +1,11 @@
+/*:
+ * @plugindesc Move with WASD and arrows. Original W (pagedown) moved to E.
+ * @help Free to use and/or modify for any project, no credit required.
+ */
+Object.assign(Input.keyMapper, {
+  //69: 'pagedown',  // E
+  82: 'up',        // R
+  68: 'left',      // D
+  70: 'down',      // F
+  71: 'right'      // G
+});
